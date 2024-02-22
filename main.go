d21e48f12ac4920164f0b68af9c720ad2d0b0846
LawrenceMueller <lawrencemueller18@gmail.com>
2024-02-21 20:20:37 -0800

    Added instructions

--- main.go
+++ main.go
@@ -7,9 +7,14 @@ import (
 func main() {
 	http.Handle("/", http.FileServer(http.Dir("./frontend")))
 
-	// Handle "/login" route specifically
+	// Handle "/lobby" route specifically
 	http.HandleFunc("/lobby", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "./frontend/login.html")
+		http.ServeFile(w, r, "./frontend/lobby.html")
+	})
+
+	// Handle "/instructions" route specifically
+	http.HandleFunc("/instructions", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./frontend/instructions.html")
 	})
 
 	http.ListenAndServe(":3000", nil)
