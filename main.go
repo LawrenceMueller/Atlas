4f6dfed6bc14451ea034ac8adf793f1fe855f776
LawrenceMueller <lawrencemueller18@gmail.com>
2024-02-21 22:35:08 -0800

    Made a bunch of progress with setting up the lobby stystem

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"context"
+	"github.com/a-h/templ"
 	"net/http"
+	"os"
 )
 
 func main() {
@@ -16,6 +19,33 @@ func main() {
 	http.HandleFunc("/instructions", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "./frontend/instructions.html")
 	})
+
+	// test data
+	testData := []string{
+		"Lobby 1: Casual Games",
+		"Lobby 2: Competitive Matches",
+		"Lobby 3: Strategy Games",
+		"Lobby 4: Racing Games",
+		"Lobby 5: Adventure Games",
+		"Lobby 6: Puzzle Games",
+		"Lobby 7: Shooter Games",
+		"Lobby 8: Sports Games",
+		"Lobby 9: Multiplayer Online Battle Arena",
+		"Lobby 10: Role-Playing Games",
+		"Lobby 11: Simulation Games",
+		"Lobby 12: Party Games",
+		"Lobby 13: Virtual Reality Games",
+		"Lobby 14: Augmented Reality Games",
+		"Lobby 15: Indie Games"}
+
+	// Route to get lobby data
+	lobbyDataComponent := lobbyData(testData)
+	http.Handle("/lobbyData", templ.Handler(lobbyDataComponent))
+	lobbyDataComponent.Render(context.Background(), os.Stdout)
+
+	// Route to confirm lobby selection
+	joinLobbyConfirmationComponent := joinLobbyConfirmation("test")
+	http.Handle("/joinLobbyConfirmation", templ.Handler(joinLobbyConfirmationComponent))
 
 	http.ListenAndServe(":3000", nil)
 }
